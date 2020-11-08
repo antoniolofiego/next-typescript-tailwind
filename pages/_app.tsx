544b72c423287446a9f2c59580158eb5bcbb0819
Antonio Lo Fiego <alofiego@hey.com>
2020-11-08 18:38:16 -0500

    Fix AppProps import for _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/styles.css';
-import type { AppProps } from 'next/app';
+import type AppProps from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
